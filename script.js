fc39ac4aab91bf594e4d8650dd3ad6a6f383552f
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-02-12 23:36:39 +0500

    Create 16 divs using javascript and arrange them in a square gride, 4 boxes in each row

---
+++ script.js
@@ -0,0 +1,15 @@
+const mainWindow = document.createElement("div");
+mainWindow.classList.add('mainWindow');
+const body=document.querySelector("body");
+body.appendChild(mainWindow);
+
+let divArray = [];
+const gridSize =16;
+for (let i=0;i<gridSize;i++)
+    {
+        divArray.push(document.createElement("div"));
+        divArray[i].classList.add('gridBox');
+        //divArray[i].textContent="how are you";
+        mainWindow.appendChild(divArray[i]);
+        console.log(divArray[i]);
+    }
