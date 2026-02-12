211a874aed7455991746af2663743190792f37eb
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-02-13 01:12:43 +0500

    Prompt the user for area of the grid and generate a grid based on that size

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const body=document.querySelector("body");
 body.appendChild(mainWindow);
 
 let divArray = [];
-const gridSize =16;
+let size = 0;
+size = prompt('Please provide gride size');
+const gridSize =size*size;
 for (let i=0;i<gridSize;i++)
     {
         divArray.push(document.createElement("div"));
         divArray[i].classList.add('gridBox');
+        divArray[i].style.width=`${800/size}px`;
+        divArray[i].style.height=`${800/size}px`;
         divArray[i].setAttribute("id",`box_${i}`);
         //divArray[i].textContent="how are you";
         mainWindow.appendChild(divArray[i]);
