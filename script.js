8eda1d39c0eac9a2a6004b9720066588debebab7
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-02-13 01:41:48 +0500

    Create a function to create the grid based on user input

--- script.js
+++ script.js
@@ -2,25 +2,46 @@ const mainWindow = document.createElement("div");
 mainWindow.classList.add('mainWindow');
 const body=document.querySelector("body");
 body.appendChild(mainWindow);
-
-let divArray = [];
-let size = 0;
-size = prompt('Please provide gride size');
-const gridSize =size*size;
-for (let i=0;i<gridSize;i++)
+function initialize()
+{
+    const sizeButton=document.querySelector("#sizeSubmit");
+    let size = 0;
+    const inputSize=document.querySelector("#size");
+    sizeButton.addEventListener("click",e=>
     {
-        divArray.push(document.createElement("div"));
-        divArray[i].classList.add('gridBox');
-        divArray[i].style.width=`${800/size}px`;
-        divArray[i].style.height=`${800/size}px`;
-        divArray[i].setAttribute("id",`box_${i}`);
-        //divArray[i].textContent="how are you";
-        mainWindow.appendChild(divArray[i]);
-        console.log(divArray[i]);
+        e.preventDefault();
+        size=inputSize.value;
+        inputSize.focus();
+        console.log(size);
+        let divArray = [];
+    const gridSize =size*size;
+    if (size>=0 && size<=100)
+    {
+        creatGrid(gridSize,divArray);
     }
+    });
+    
+}
+function creatGrid(gridSize,divArray)
+{
+    for (let i=0;i<gridSize;i++)
+        {
+
+            divArray.push(document.createElement("div"));
+            divArray[i].classList.add('gridBox');
+            divArray[i].style.width=`${800/size}px`;
+            divArray[i].style.height=`${800/size}px`;
+            divArray[i].setAttribute("id",`box_${i}`);
+            //divArray[i].textContent="how are you";
+            mainWindow.appendChild(divArray[i]);
+            console.log(divArray[i]);
+
+        }
+}
+initialize();
 mainWindow.addEventListener('mouseover',e =>
 {
     if (!e.target.classList.contains('gridBox'))
         return;
     e.target.style.backgroundColor='aqua';
-})
+});
