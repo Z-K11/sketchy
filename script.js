2481d181eec1e56d177dd0007e198c0e4becdd22
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-02-13 15:30:11 +0500

    Add the functionality where providing a new size for the grid creates a new gride without throwing any errors

--- script.js
+++ script.js
@@ -2,47 +2,64 @@ const mainWindow = document.createElement("div");
 mainWindow.classList.add('mainWindow');
 const body=document.querySelector("body");
 body.appendChild(mainWindow);
-function initialize()
-{
-    const sizeButton=document.querySelector("#sizeSubmit");
-    let size = 0;
-    const inputSize=document.querySelector("#size");
-    sizeButton.addEventListener("click",e=>
-    {
-        e.preventDefault();
-        size=inputSize.value;
-        inputSize.focus();
-        console.log(size);
-        let divArray = [];
-    const gridSize =size*size;
-    if (size>=0 && size<=100)
-    {
-        creatGrid(gridSize,divArray,size);
-    }
-    });
-    
-}
+reset = false;
+
 function creatGrid(gridSize,divArray,size)
 {
     for (let i=0;i<gridSize;i++)
         {
 
-            divArray.push(document.createElement("div"));
+            divArray[i]=document.createElement("div");
             divArray[i].classList.add('gridBox');
             divArray[i].style.width=`${800/size}px`;
             divArray[i].style.height=`${800/size}px`;
             divArray[i].setAttribute("id",`box_${i}`);
             //divArray[i].textContent="how are you";
             mainWindow.appendChild(divArray[i]);
-            console.log(divArray[i]);
 
         }
 }
-initialize();
+function destroyGrid(divArray)
+{
+    for (let i=0;i<divArray.length;i++)
+    {
+        divArray[i].remove();
+    }
+    divArray.length=0;
+}
+const sizeButton=document.querySelector("#sizeSubmit");
+let size = 0;
+let divArray = [];
+const inputSize=document.querySelector("#size");
+sizeButton.addEventListener("click",e=>
+    {
+        e.preventDefault();
+        size=inputSize.value;
+        inputSize.focus();
+        console.log(size);
+        const gridSize =size*size;
+        switch (reset) {
+            case false:
+                if (size>=0 && size<=100)
+                {
+                    creatGrid(gridSize,divArray,size);
+                    reset = true;
+                }
+                break;
+            case true:
+                destroyGrid(divArray);
+                if(size>=0 && size<=100)
+                    creatGrid(gridSize,divArray,size);
+                break;
+            default:
+                alert("Error in grid creation or destruction");
+                break;
+        }
+       
+    });
 mainWindow.addEventListener('mouseover',e =>
 {
     if (!e.target.classList.contains('gridBox'))
         return;
     e.target.style.backgroundColor='aqua';
-    console.log(e.target.id);
 });
